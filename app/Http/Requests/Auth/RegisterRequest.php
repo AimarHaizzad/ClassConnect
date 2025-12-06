0f4a1f283e1b817aa9f8a2c535e4da1292a4097e
NabihahFatiny <nurnabihahfatiny545@gmail.com>
2025-12-06 17:19:51 +0800

    update prevents duplicate accounts

--- app/Http/Requests/Auth/RegisterRequest.php
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -3,7 +3,6 @@
 namespace App\Http\Requests\Auth;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rules\Password;
 
 class RegisterRequest extends FormRequest
 {
@@ -20,7 +19,7 @@ class RegisterRequest extends FormRequest
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email'],
             'mobile_phone' => ['required', 'string', 'max:20'],
             'date_of_birth' => ['required', 'date', 'before:today'],
-            'password' => ['required', 'string', Password::defaults()],
+            'password' => ['required', 'string', 'min:6'],
             'user_id' => ['required', 'string', 'max:255', 'unique:users,user_id'],
             'user_type' => ['nullable', 'in:student,lecturer'],
         ];
